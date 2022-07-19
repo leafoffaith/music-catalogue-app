c5f9bf9ad661859b80d7a235312354adfd2e1906
leafoffaith <shaurya26@gmail.com>
2022-07-19 15:50:05 +0530

    formatted app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'; 
+import { useState, useEffect } from 'react';
 import { accessToken } from './spotify'
 import logo from './logo.svg';
 import './App.css';
@@ -6,41 +6,37 @@ import './App.css';
 
 function App() {
 
-  const [token, setToken] = useState(null);
+    const [token, setToken] = useState(null);
 
-  useEffect(() => {
+    useEffect(() => {
 
-    setToken(accessToken);
-    //set to the accessToken variable 'imported' above
-
-
-    // console.log(accessToken)
-    // console.log(refreshToken)
-    
-    // if(refreshToken) {
-    //   fetch(`http://localhost:8888/refresh_token?refresh_token=${refreshToken}`)
-    //   .then(res => res.json())
-    //   .then(data => console.log(data))
-    //   .catch(err => console.log(err))
-    // }
-
-  }, [])
+        setToken(accessToken);
+        //set to the accessToken variable 'imported' above
 
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        if(!token ?(
-          <a
-          className="App-link"
-          href="http://localhost:8888/login"
-        >
-          Log in to Spotify
-        </a>
-        )) : <h1>Logged in</h1>
-      </header>
-    </div>
-  );
+        // console.log(accessToken)
+        // console.log(refreshToken)
+
+        // if(refreshToken) {
+        //   fetch(`http://localhost:8888/refresh_token?refresh_token=${refreshToken}`)
+        //   .then(res => res.json())
+        //   .then(data => console.log(data))
+        //   .catch(err => console.log(err))
+        // }
+
+    }, [])
+
+
+    return (
+    <div className="App" >
+        <header className="App-header" >
+            if (!token ? ( < a className="App-link"
+                href="http://localhost:8888/login" >
+                Log in to Spotify </a>
+                )): <h1> Logged in </h1>   
+                </header> 
+                </div>
+                    );
 }
 
-export default App;
+                    export default App;
