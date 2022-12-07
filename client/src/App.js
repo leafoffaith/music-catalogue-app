1b5d2a9f1b6f7d5347962f38f5bcac459a90fcf3
Shaur <leafoffaith26@gmail.com>
2022-12-07 10:15:02 +0000

    Spotify Auth Implemented, Fetching and displaying user data

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,73 @@
 import { useState, useEffect } from 'react';
-import { accessToken } from './spotify'
+import { accessToken, logout, getCurrentUserProfile } from './spotify'
 import logo from './logo.svg';
 import './App.css';
+import { catchErrors } from './utils';
 
 
 function App() {
 
-    const [token, setToken] = useState(null);
+  const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
-    useEffect(() => {
+  useEffect(() => {
 
-        setToken(accessToken);
-        //set to the accessToken variable 'imported' above
+    setToken(accessToken);
+    //set to the accessToken variable 'imported' above
+
+    //Since getCurrentUserProfile() returns a promise, we need to wait for the promise to be resolved using await.
+    //Since the await operator can only be used inside async functions, we handle this by creating an async function called fetchData() within our useEffect hook and invoking it. 
+    //It's cleanest to do it this way because making the useEffect hook itself async gets hairy pretty quickly.
+    const fetchData = async () => {
+        const { data } = await getCurrentUserProfile();
+        setProfile(data);
+        console.log(data)
+    };
+    catchErrors(fetchData());
+
+    // console.log(accessToken)
+    // console.log(refreshToken)
+
+    // if(refreshToken) {
+    //   fetch(`http://localhost:8888/refresh_token?refresh_token=${refreshToken}`)
+    //   .then(res => res.json())
+    //   .then(data => console.log(data))
+    //   .catch(err => console.log(err))
+    // }
+
+  }, [])
+
+  // The second argument of useEffect function is referred to as the “dependency array”. When the variable included inside the array didn’t change, the function passed as the first argument won’t be executed.
 
 
-        // console.log(accessToken)
-        // console.log(refreshToken)
-
-        // if(refreshToken) {
-        //   fetch(`http://localhost:8888/refresh_token?refresh_token=${refreshToken}`)
-        //   .then(res => res.json())
-        //   .then(data => console.log(data))
-        //   .catch(err => console.log(err))
-        // }
-
-    }, [])
-
-
-    return (
+  return (
     <div className="App" >
-        <header className="App-header" >
-            if (!token ? ( < a className="App-link"
-                href="http://localhost:8888/login" >
-                Log in to Spotify </a>
-                )): <h1> Logged in </h1>   
-                </header> 
-                </div>
-                    );
+      {/* <button onClick={decrementCount}>-</button>
+          <span>{count}</span> */}
+      <header className="App-header" >
+        {!token ? (<a
+          className="App-link"
+          href="http://localhost:8888/login"> Log in to Spotify
+        </a>) : (
+          <>
+            <h1>Logged in!</h1>
+            <button onClick={logout}>Log Out</button>
+            {profile && (
+              <div>
+                <h1>{profile.display_name}</h1>
+                <p>{profile.followers.total} Followers</p>
+                {profile.images.length && profile.images[0].url && (
+                  <img src={profile.images[0].url} alt="Profile" />
+                )}
+              </div>
+
+            )
+            }
+          </>
+        )}
+      </header>
+    </div>
+  );
 }
 
-                    export default App;
+export default App;
--- client/src/spotify.js
+++ client/src/spotify.js
@@ -1,10 +1,144 @@
-const getAccessToken = () => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get('access_token')
-    const refreshToken = urlParams.get('refresh_token');
+import axios from "axios";
+//
 
-    return accessToken;
-}
+//Map for localStorage keys
+const LOCALSTORAGE_KEYS = {
+  accessToken: "spotify_access_token",
+  refreshToken: "spotify_refresh_token",
+  expireTime: "spotify_token_expire_time",
+  timestamp: "spotify_token_timestamp",
+};
+
+//Map to retrieve LocalStorage values
+const LOCALSTORAGE_VALUES = {
+  accessToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.accessToken),
+  refreshToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.refreshToken),
+  expireTime: window.localStorage.getItem(LOCALSTORAGE_KEYS.expireTime),
+  timestamp: window.localStorage.getItem(LOCALSTORAGE_KEYS.timestamp),
+};
+
+const getAccessToken = () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const queryParams = {
+    [LOCALSTORAGE_KEYS.accessToken]: urlParams.get("access_token"),
+    [LOCALSTORAGE_KEYS.refreshToken]: urlParams.get("refresh_token"),
+    [LOCALSTORAGE_KEYS.expireTime]: urlParams.get("expires_in"),
+  };
+  const hasError = urlParams.get("error");
+
+  if (
+    hasError ||
+    hasTokenExpired() ||
+    LOCALSTORAGE_VALUES.accessToken === "undefined"
+  ) {
+    refreshToken();
+  }
+
+  //If there is a token in the URL query params, user is logged in
+  if (queryParams[LOCALSTORAGE_KEYS.accessToken]) {
+    //Store the queryParams in localStorage
+    for (const property in queryParams) {
+      window.localStorage.setItem(property, queryParams[property]);
+    }
+    //set timestamp
+    window.localStorage.setItem(LOCALSTORAGE_KEYS.timestamp, Date.now());
+    // Return access token from query params
+    return queryParams[LOCALSTORAGE_KEYS.accessToken];
+  }
+
+  // We should never get here
+  return false;
+};
+
+// const accessToken = urlParams.get('access_token')
+// const refreshToken = urlParams.get('refresh_token');
+
+// return accessToken;
+
+//Helper function
+
+/**
+ * Checks if the amount of time that has elapsed between the timestamp in localStorage
+ * and now is greater than the expiration time of 3600 seconds (1 hour).
+ * @returns {boolean} Whether or not the access token in localStorage has expired
+ */
+
+const hasTokenExpired = () => {
+  const { accessToken, timestamp, expireTime } = LOCALSTORAGE_VALUES;
+  if (!accessToken || !timestamp) {
+    return false;
+  }
+  const millisecondsElapsed = Date.now() - Number(timestamp);
+  return millisecondsElapsed / 1000 > Number(expireTime);
+};
+
+/**
+ * Use the refresh token in localStorage to hit the /refresh_token endpoint
+ * in our Node app, then update values in localStorage with data from response.
+ * @returns {void}
+ */
+const refreshToken = async () => {
+  try {
+    // Logout if there's no refresh token stored or we've managed to get into a reload infinite loop
+    if (
+      !LOCALSTORAGE_VALUES.refreshToken ||
+      LOCALSTORAGE_VALUES.refreshToken === "undefined" ||
+      Date.now() - Number(LOCALSTORAGE_VALUES.timestamp) / 1000 < 1000
+    ) {
+      console.error("No refresh token available");
+      logout();
+    }
+
+    // Use `/refresh_token` endpoint from our Node app
+    const { data } = await axios.get(
+      `/refresh_token?refresh_token=${LOCALSTORAGE_VALUES.refreshToken}`
+    );
+
+    // Update localStorage values
+    window.localStorage.setItem(
+      LOCALSTORAGE_KEYS.accessToken,
+      data.access_token
+    );
+    window.localStorage.setItem(
+        LOCALSTORAGE_KEYS.timestamp,
+        Date.now()
+    );
+
+    // Reload the page for localStorage updates to be reflected
+    window.location.reload();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+/**
+ * Clear out all localStorage items we've set and reload the page
+ * @returns {void}
+ */
+export const logout = () => {
+  // Clear all localStorage items
+  for (const property in LOCALSTORAGE_KEYS) {
+    window.localStorage.removeItem(LOCALSTORAGE_KEYS[property]);
+  }
+  // Navigate to homepage
+  window.location = window.location.origin;
+};
 
 export const accessToken = getAccessToken();
+
+/**
+ * Axios global request headers
+ * https://github.com/axios/axios#global-axios-defaults
+ */
+ axios.defaults.baseURL = 'https://api.spotify.com/v1';
+ axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+ axios.defaults.headers['Content-Type'] = 'application/json';
+
+ /**
+  * Get Current User's Profile
+  * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-current-users-profile
+  * @returns {Promise}
+  */
+
+ export const getCurrentUserProfile = () => axios.get('/me');
