c97b7d978039bddd43191e1a8de5ed6bf4f79726
Shaur <leafoffaith26@gmail.com>
2022-12-07 13:23:09 +0000

    Added React-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,12 @@ import { accessToken, logout, getCurrentUserProfile } from './spotify'
 import logo from './logo.svg';
 import './App.css';
 import { catchErrors } from './utils';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 
 function App() {
@@ -19,9 +25,9 @@ function App() {
     //Since the await operator can only be used inside async functions, we handle this by creating an async function called fetchData() within our useEffect hook and invoking it. 
     //It's cleanest to do it this way because making the useEffect hook itself async gets hairy pretty quickly.
     const fetchData = async () => {
-        const { data } = await getCurrentUserProfile();
-        setProfile(data);
-        console.log(data)
+      const { data } = await getCurrentUserProfile();
+      setProfile(data);
+      console.log(data)
     };
     catchErrors(fetchData());
 
@@ -49,21 +55,41 @@ function App() {
           className="App-link"
           href="http://localhost:8888/login"> Log in to Spotify
         </a>) : (
-          <>
-            <h1>Logged in!</h1>
-            <button onClick={logout}>Log Out</button>
-            {profile && (
-              <div>
-                <h1>{profile.display_name}</h1>
-                <p>{profile.followers.total} Followers</p>
-                {profile.images.length && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt="Profile" />
-                )}
-              </div>
+          <Router>
+            <div>
+              <Link to="/">Home</Link>
+              <Link to="/top-artists">Top Artists</Link>
+              <Link to="/top-tracks">Top Tracks</Link>
+            </div>
+            <Switch>
+              <Route path="/top-artists">
+                <h1>Top Artists</h1>
+              </Route>
+              <Route path="/top-tracks">
+                <h1>Top Tracks</h1>
+              </Route>
+              <Route path="/top-artists">
+                <h1>Top Playlists</h1>
+              </Route>
 
-            )
-            }
-          </>
+              <Route path="/">
+                <>
+                  <h1>Logged in!</h1>
+                  <button onClick={logout}>Log Out</button>
+                  {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Profile" />
+                      )}
+                    </div>
+                  )
+                  }
+                </>
+              </Route>
+            </Switch>
+          </Router>
         )}
       </header>
     </div>
